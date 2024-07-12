b83e9af0d9dc3dc658c844712aeb50d200355483
Kouagou Tiburce <kouagoutiburce@gmail.com>
2024-07-12 17:48:33 +0200

    TodoMain créé

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,18 @@
+<template>
+  <li :class="{ completed: todo.complete }">
+    <div class="view">
+      <input type="checkbox" class="toggle" />
+      <label for="">{{ todo.title }}</label>
+      <button class="destroy"></button>
+    </div>
+  </li>
+</template>
+
+<script setup lang="ts">
+import type { Todo } from '@/@types'
+defineProps<{
+  todo: Todo
+}>()
+</script>
+
+<style scoped></style>
---
+++ src/components/TodoMain.vue
@@ -0,0 +1,19 @@
+<template>
+  <main class="main">
+    <ul class="todo-list">
+      <TodoItem v-for="todo in taches" :key="todo.id" :todo="todo" />
+    </ul>
+  </main>
+</template>
+
+<script setup lang="ts">
+import type { Todo } from '@/@types'
+import TodoItem from '@/components/TodoItem.vue'
+// const props = defineProps(['taches']);
+
+const props = defineProps<{
+  taches: Todo[]
+}>()
+</script>
+
+<style scoped></style>
