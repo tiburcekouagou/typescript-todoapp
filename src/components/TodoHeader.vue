fe855ec2f9a508765ee7a30e332e7b03e171ad2e
Kouagou Tiburce <kouagoutiburce@gmail.com>
2024-07-12 16:46:59 +0200

    TodoHeader créé

---
+++ src/components/TodoHeader.vue
@@ -0,0 +1,19 @@
+<template>
+  <header class="header">
+    <RouterLink to="/">
+      <h1>todos</h1>
+    </RouterLink>
+
+    <input
+      class="new-todo"
+      type="text"
+      placeholder="Entrez une tâche"
+      autofocus
+      autocomplete="off"
+    />
+  </header>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
